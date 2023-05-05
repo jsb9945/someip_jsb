67797a449b509eee73c0b96d123e05b365945200
jsb9945 <bin90405@naver.com>
2023-05-05 14:21:01 +0900

    dd

--- someip/client_udp.cpp
+++ someip/client_udp.cpp
@@ -9,16 +9,22 @@ using namespace std;
 
 // Some/IP 헤더 구조체 정의
 struct someip_header_t {
-    uint32_t message_id;
+    //uint32_t message_id;
     uint32_t length;
     uint32_t request_id;
     uint8_t protocol_version;
     uint8_t interface_version;
     uint8_t message_type;
-    uint8_t reserved;
+    //uint8_t reserved;
     uint16_t method_id;
     uint16_t client_id;
     uint16_t session_id;
+};
+
+// 위도, 경도 구조체
+struct location_t {
+    double latitude;
+    double longitude;
 };
 
 // Some/IP 통신을 담당하는 클래스 정의
@@ -37,27 +43,49 @@ public:
         }
     }
 
-    void send(const string& message_data) {
+
+    void send(const location_t& message_data) {
         // Some/IP Message 생성
         someip_header_t header;
         memset(&header, 0, sizeof(header));
-        header.length = message_data.length() + sizeof(header);
+        header.length = sizeof(message_data) + sizeof(header);
         header.protocol_version = 1;
         header.interface_version = 1;
-        header.message_type = 0;
+        header.request_id = 8;
+        header.message_type = 0x00; // Request
         header.method_id = method_id_;
         header.client_id = 0;
         header.session_id = 0;
         
-        char buffer[sizeof(header) + message_data.length()];
+        char buffer[sizeof(header) + sizeof(message_data)];
         memcpy(buffer, &header, sizeof(header));
-        memcpy(buffer + sizeof(header), message_data.c_str(), message_data.length());
+        memcpy(buffer + sizeof(header), &message_data, sizeof(message_data));
 
         // Some/IP Message를 서버에 전송합니다.
         if (sendto(socket_, buffer, sizeof(buffer), 0, (struct sockaddr*)&server_address_, sizeof(server_address_)) < 0) {
             cerr << "Error: Failed to send message" << endl;
         }
+
+        char recv_buffer[1024];
+        socklen_t server_address_len = sizeof(server_address_);
+        ssize_t recv_len = recvfrom(socket_, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr*)&server_address_, &server_address_len);
+        if(recv_len<0){
+            cerr << "Error: Failed to receive message" << endl;
+        }
+        else{
+            someip_header_t recv_header;
+            memcpy(&recv_header, recv_buffer, sizeof(recv_header));
+            if(recv_header.request_id != header.request_id){
+                cerr << "Error: Not same request_id" << endl;
+            }
+            else{
+                string recv_data(recv_buffer + sizeof(recv_header), recv_len - sizeof(recv_header));
+                cout << "Recv : " << recv_data << endl;    
+            }
+    
+        }
     }
+
 
 private:
     int socket_;
@@ -67,11 +95,26 @@ private:
 };
 
 int main() {
+    double lat, lon;
+    location_t data;
+
     // Some/IP 클라이언트 객체 생성
-    AutosarSomeIpClient client("192.168.0.153", 3000, 0x1234, 0x5680);
+    AutosarSomeIpClient client("192.168.0.153", 3000, 0x1234, 0x5678);
 
     // Some/IP Message 전송
-    client.send("Hello, world!");
+    while(1){
+        location_t data = {0.0, 0.0};
+        cout << "lat: ";
+        cin >> lat;
+        data.latitude = lat;
+        cout << "lon: ";
+        cin >> lon;
+        data.longitude = lon;
+        
+        client.send(data);
+
+    }
+    
 
     return 0;
 }
--- someip/server_udp.cpp
+++ someip/server_udp.cpp
@@ -4,22 +4,33 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <cmath>
 
 using namespace std;
 
 // Some/IP 헤더 구조체 정의
 struct someip_header_t {
-    uint32_t message_id;
+    //uint32_t message_id;
     uint32_t length;
     uint32_t request_id;
     uint8_t protocol_version;
     uint8_t interface_version;
     uint8_t message_type;
-    uint8_t reserved;
+    //uint8_t reserved;
     uint16_t method_id;
     uint16_t client_id;
     uint16_t session_id;
 };
+
+// 위도, 경도 구조체
+struct location_t {
+    double latitude;
+    double longitude;
+};
+
+double degreeToRadian(double degree);
+double radianToDegree(double radian);
+double getBearing(double lat1, double lng1, double lat2, double lng2);
 
 // Some/IP 통신을 담당하는 클래스 정의
 class AutosarSomeIpServer {
@@ -65,8 +76,48 @@ public:
             }
 
             // Some/IP Message 처리
-            string message_data(buffer + sizeof(header), recv_len - sizeof(header));
-            cout << "Received message: " << message_data << endl;
+            location_t message_data;
+            memcpy(&message_data, buffer + sizeof(header), sizeof(message_data));
+            
+            double lat = message_data.latitude;
+            double lon = message_data.longitude;
+            double target_lat = 37.5665;
+            double target_lon = 126.9780;
+            cout << "latitude: " << lat << "\tlongitude: " << lon << endl;
+            //여기까진 문제 없음
+
+
+            // 위도 경도로 방향 알려주는 알고리즘
+            double bearing = getBearing(lat, lon, target_lat, target_lon);
+            string message_data1 = to_string(bearing);
+
+            //string message_data1 = "Hello World";
+            send_response(socket_, &client_address, client_address_len, header.request_id, message_data1);
+        }
+    }
+
+    void send_response(int socket, struct sockaddr_in* client_address_, socklen_t client_address_len_, uint32_t request_id_, string& message_data_) {
+        // Some/IP Response Message 구성
+        someip_header_t header;
+        memset(&header, 0, sizeof(header));
+        //header.message_id = message_id_;
+        header.length = sizeof(header) + sizeof(message_data_);
+        //header.request_id = request_id_;
+        header.request_id = request_id_;
+        header.protocol_version = 1;
+        header.interface_version = 1;
+        header.message_type = 0x02;  // Response
+        header.method_id = 0;
+        header.client_id = 0;
+        header.session_id = 0;
+
+        // Some/IP Response Message 전송
+        char buffer[1024];
+        memset(&buffer, 0, sizeof(buffer));
+        memcpy(buffer, &header, sizeof(header));
+        memcpy(buffer + sizeof(header), message_data_.c_str(), message_data_.length());
+        if (sendto(socket, buffer, header.length, 0, (struct sockaddr*)client_address_, client_address_len_) < 0) {
+            cerr << "Error: Failed to send response message" << endl;
         }
     }
 
@@ -85,4 +136,24 @@ int main() {
     server.receive();
 
     return 0;
+}
+
+double degreeToRadian(double degree) {
+    return degree * M_PI / 180.0;
+}
+
+double radianToDegree(double radian) {
+    return radian * 180.0 / M_PI;
+}
+
+double getBearing(double lat1, double lng1, double lat2, double lng2) {
+    double dLon = degreeToRadian(lng2 - lng1);
+    double y = sin(dLon) * cos(degreeToRadian(lat2));
+    double x = cos(degreeToRadian(lat1)) * sin(degreeToRadian(lat2)) -
+               sin(degreeToRadian(lat1)) * cos(degreeToRadian(lat2)) * cos(dLon);
+    double bearing = radianToDegree(atan2(y, x));
+    if (bearing < 0) {
+        bearing += 360.0;
+    }
+    return bearing;
 }
